2d1956ce201e9a8c2fa695f99feba045d0d8e8fa
julio_filizzola <juliofilizzola@hotmail.com>
2024-11-17 19:17:51 -0300

    Fix QR code creation to use provided data
    Previously, the QR code was being generated with a hardcoded string. This commit updates the function to encode the actual data passed as an argument, ensuring it reflects the provided input correctly.

--- lib/service/QrCode.py
+++ lib/service/QrCode.py
@@ -2,7 +2,8 @@ from PIL import Image
 from pylibdmtx.pylibdmtx import encode
 
 def create_qr_code(data: str) -> Image:
-    encoded = encode('hello world'.encode('utf8'))
+    print(data, 'data')
+    encoded = encode(data.encode('utf8'))
     img = Image.frombytes('RGB', (encoded.width, encoded.height), encoded.pixels)
     img.save('dmtx.png')
     return img
